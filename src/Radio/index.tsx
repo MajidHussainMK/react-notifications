c34682600d871d8e83dabd8e3a979825c134b556
Majid Hussain <majid.hussain.mk@gmail.com>
2021-11-21 23:49:58 +0500

    configure Radio component to handle checkbox case

--- src/Radio/index.tsx
+++ src/Radio/index.tsx
@@ -6,21 +6,33 @@ export const Radio: React.FC<{
   label: string;
   value: string;
   defaultChecked?: boolean;
+  type?: "checkbox" | "radio";
   onClick?: (value: string) => void;
   css?: CSSProperties;
-}> = ({ label, name, value, id = value, css, defaultChecked, onClick }) => {
+}> = ({
+  css,
+  name,
+  label,
+  value,
+  id = value,
+  type = "radio",
+  defaultChecked,
+  onClick,
+}) => {
   return (
     <div style={{ marginRight: 8, ...css }}>
       <input
         id={id}
         name={name}
-        type="radio"
+        type={type}
         value={value}
         defaultChecked={defaultChecked}
         onChange={({ target }) => onClick?.(target.value)}
         style={{ marginRight: 8 }}
       />
-      <label htmlFor={id}>{label}</label>
+      <label htmlFor={id} style={{ cursor: "pointer" }}>
+        {label}
+      </label>
     </div>
   );
 };
